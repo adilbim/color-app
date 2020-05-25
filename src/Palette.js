c1272bdfc1956d0039427439251877069aa51b69
adil bimzagh <abshow15@gmail.com>
2020-05-25 12:46:08 +0000

    create-react-app and add seedColors

---
+++ src/Palette.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+
+class Palette extends Component{
+
+
+    render(){
+        return(
+            <div className="Palette">
+                {/* {here goes the nav bar} */}
+                <div className="Palette-colors"></div>
+                {/* {here goes the footer} */}
+            </div>
+        );
+    }
+}
+
+
+export default Palette;
