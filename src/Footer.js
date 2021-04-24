07d210ad2ae6a32ecca303aca41fa7e9cf96f4c7
Adil Bimzagh <abshow15@gmail.com>
2021-04-24 15:45:27 +0000

    Prettify Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
-
-function Footer(props){
-   const {paletteName, emoji} = props;
-    return (
-        <footer className="palette-footer">
-            <span>{paletteName}</span>
-            <span className="emoji">{emoji}</span>
-        </footer>
-    );
+function Footer(props) {
+  const { paletteName, emoji } = props;
+  return (
+    <footer className="palette-footer">
+      <span>{paletteName}</span>
+      <span className="emoji">{emoji}</span>
+    </footer>
+  );
 }
 
 export default Footer;
