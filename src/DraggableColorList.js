80975b462cf82b915cc7bc6ee2649cc19622cb23
Adil Bimzagh <abshow15@gmail.com>
2021-04-24 15:49:22 +0000

    Prettify DraggableColor List/Box .js

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import DraggableColorBox from './DraggableColorBox';
-import { SortableContainer} from "react-sortable-hoc";
-
+import { SortableContainer } from 'react-sortable-hoc';
 
 const DraggableColorList = SortableContainer(({ colors, deleteColorBox }) => {
-    return (
-      <div style={{height: "100%"}}>
-        {colors.map((color, index) => (
-          <DraggableColorBox key={color.name} index={index} handleClick={()=> deleteColorBox(color.name)} color={color}/>
-        ))}
-      </div>
-    );
-  });
+  return (
+    <div style={{ height: '100%' }}>
+      {colors.map((color, index) => (
+        <DraggableColorBox
+          key={color.name}
+          index={index}
+          handleClick={() => deleteColorBox(color.name)}
+          color={color}
+        />
+      ))}
+    </div>
+  );
+});
 
-
- export default DraggableColorList; 
+export default DraggableColorList;
