7e1a5dcc5e0df7af63fdfe7d265a5850138408eb
Adil Bimzagh <abshow15@gmail.com>
2021-04-24 15:44:33 +0000

    Prettify MiniPalette.js

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,67 +2,65 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
-    root:{
-        backgroundColor: "white",
-        border: "1px solid black",
-        borderRadius: "5px",
-        padding: "0.5rem",
-        position: "relative",
-        overflow: "hidden",
-        "&:hover": {
-          cursor: "pointer"
-        }
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
     },
-    colors: {
-        backgroundColor: "#dae1e4",
-        height: "150px",
-        width: "100%",
-        borderRadius: "5px",
-        overflow: "hidden"
-      },
-      title: {
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        margin: "0",
-        color: "black",
-        paddingTop: "0.5rem",
-        fontSize: "1rem",
-        position: "relative"
-      },
-      emoji: {
-        marginLeft: "0.5rem",
-        fontSize: "1.5rem"
-      },
-      miniColor: {
-        height: "25%",
-        width: "20%",
-        display: "inline-block",
-        margin: "0 auto",
-        position: "relative",
-        marginBottom: "-3.5px"
-      }
-}
+  },
+  colors: {
+    backgroundColor: '#dae1e4',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative',
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem',
+  },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-3.5px',
+  },
+};
 
-function MiniPalette(props){ 
-    
-    const {paletteName, emoji, classes, colors} = props;
-    const miniColorBoxes = colors.map(color => (
-        <div className={classes.miniColor} style={{background: color.color}} key={color.color}>
-
-        </div>
-    ))
-    return (
-        <div className={classes.root} onClick={props.handleClick}>
-            <div className={classes.colors}>
-                {miniColorBoxes}
-            </div>
-            <h5 className={classes.title}>
-                {paletteName} <span className={classes.emoji} >{emoji}</span>
-            </h5>
-        </div>
-    );
-    
+function MiniPalette(props) {
+  const { paletteName, emoji, classes, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ background: color.color }}
+      key={color.color}
+    ></div>
+  ));
+  return (
+    <div className={classes.root} onClick={props.handleClick}>
+      <div className={classes.colors}>{miniColorBoxes}</div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+  );
 }
 
 export default withStyles(styles)(MiniPalette);
