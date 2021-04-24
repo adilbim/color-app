8c12b790f00545d9a0bccd923f22f38673412023
Adil Bimzagh <abshow15@gmail.com>
2021-04-24 13:19:32 +0000

    Remake the App component with hooks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
-import Palette from './Palette';
-import PaletteList from './PaletteList';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
-import SingleColorPalette from './SingleColorPalette';
-import NewPaletteForm from './NewPaletteForm';
+import Palette from "./Palette";
+import PaletteList from "./PaletteList";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
+import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 
+function App() {
+  const [palettes, setPalettes] = useState(seedColors);
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {palettes: seedColors};
-    this.findPalette = this.findPalette.bind(this);
-    this.addNewPalette = this.addNewPalette.bind(this);
-  }
+  const findPalette = (id) => {
+    return palettes.find((palette) => palette.id === id);
+  };
 
-  findPalette(id){
-    return this.state.palettes.find((palette)=> palette.id === id);
-  }
-  
-  addNewPalette(newPalette){
-   this.setState({palettes: [...this.state.palettes, newPalette]});
-  }
+  const addNewPalette = (newPalette) => {
+    setPalettes([...palettes, newPalette]);
+  };
 
-  render(){ 
   return (
     <Switch>
-      <Route exact path="/" 
-      render={(routProps)=> <PaletteList palettes={this.state.palettes} {...routProps} />}
+      <Route
+        exact
+        path="/"
+        render={(routProps) => (
+          <PaletteList palettes={palettes} {...routProps} />
+        )}
       />
-      <Route exact path="/palette/new" render={(routeProps)=>
-          <NewPaletteForm 
-            palettes={this.state.palettes} 
-            {...routeProps} 
-            addNewPalette={this.addNewPalette} 
-          />}
+      <Route
+        exact
+        path="/palette/new"
+        render={(routeProps) => (
+          <NewPaletteForm
+            palettes={palettes}
+            {...routeProps}
+            addNewPalette={addNewPalette}
+          />
+        )}
       />
-      <Route 
-      exact 
-      path="/palette/:id" 
-      render={(routeProps)=> <Palette Palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} 
+      <Route
+        exact
+        path="/palette/:id"
+        render={(routeProps) => (
+          <Palette
+            Palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
       />
-      <Route exact path="/palette/:idPalette/:idColor" render={(routeProps)=> 
-      <SingleColorPalette 
-        {...routeProps} 
-        Palette={generatePalette(this.findPalette(routeProps.match.params.idPalette))} 
-        />}
+      <Route
+        exact
+        path="/palette/:idPalette/:idColor"
+        render={(routeProps) => (
+          <SingleColorPalette
+            {...routeProps}
+            Palette={generatePalette(
+              findPalette(routeProps.match.params.idPalette)
+            )}
+          />
+        )}
       />
     </Switch>
   );
-}
 }
 
 export default App;
