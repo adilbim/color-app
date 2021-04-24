d759c9ced19527d01156dd452035f84762e39b69
Adil Bimzagh <abshow15@gmail.com>
2021-04-24 15:40:11 +0000

    Remake SingleColorPalette.js with hooks

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,59 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 
+function SingleColorPalette({ Palette, match }) {
+  const [format, setFormat] = useState('hex');
 
-
-
-class SingleColorPalette extends React.Component{
-    constructor(props){
-        super(props);
-        this._shades = this._shades.bind(this);
-        this.state = {format: 'hex'};
-        this.changeFormat = this.changeFormat.bind(this);
+  const _shades = (id) => {
+    let shades = [];
+    let colors = Palette.colors;
+    for (let key in colors) {
+      shades = shades.concat(colors[key].filter((color) => color.id === id));
     }
-    
-    _shades(id){
-        let shades = [];
-        let colors= this.props.Palette.colors;
-        for(let key in colors){
-            shades = shades.concat(
-               colors[key].filter(color => color.id === id)
-            );
-         }
-        return shades.slice(1);
-    }
+    return shades.slice(1);
+  };
 
-    changeFormat(newFormat){
-        this.setState({format: newFormat});
-     }
+  const { idColor } = match.params;
+  const { paletteName, emoji, id } = Palette;
+  const shadesOfColor = _shades(idColor).map((color, indx) => (
+    <ColorBox key={indx} name={color.name} color={color[format]} />
+  ));
 
-    render(){
-        const {idColor} = this.props.match.params;
-        const {paletteName, emoji, id} = this.props.Palette;
-        const {format} = this.state;
-        const shadesOfColor = this._shades(idColor).map((color,indx) =>
-         <ColorBox key={indx} 
-         name={color.name} 
-         color={color[format]} 
-         />)
-        return (
-            <div className='Palette SingleColorPalette'>
-               <Navbar  changeFormat={this.changeFormat} showSlider={false} />
-               <div className='Palette-colors'>
-                   {shadesOfColor}
-                   <div className="ColorBox go-back">
-                        <Link to={`/palette/${id}`}>  
-                           <span className="copy-button">Go Back</span> 
-                        </Link>
-                   </div>   
-                </div>
-               <Footer paletteName={paletteName} emoji={emoji} />
-            </div>
-        );
-    }
+  return (
+    <div className="Palette SingleColorPalette">
+      <Navbar changeFormat={setFormat} showSlider={false} />
+      <div className="Palette-colors">
+        {shadesOfColor}
+        <div className="ColorBox go-back">
+          <Link to={`/palette/${id}`}>
+            <span className="copy-button">Go Back</span>
+          </Link>
+        </div>
+      </div>
+      <Footer paletteName={paletteName} emoji={emoji} />
+    </div>
+  );
 }
 
 export default SingleColorPalette;
