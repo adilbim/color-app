50bab37c862b19bc146e1a66c91c43367d1b3d36
Adil Bimzagh <abshow15@gmail.com>
2021-04-25 15:25:49 +0000

    Updates

---
+++ precache-manifest.97c04408e91f5ce2d0cfe17f8e6fc70f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b55c46b22e49bb99f4532ab4d9d10c",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "af9f3b014560f6d98d4a",
+    "url": "/color-app/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "169811dfcf55591403e3",
+    "url": "/color-app/static/css/main.bec2b5f1.chunk.css"
+  },
+  {
+    "revision": "af9f3b014560f6d98d4a",
+    "url": "/color-app/static/js/2.73a3c6e1.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-app/static/js/2.73a3c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169811dfcf55591403e3",
+    "url": "/color-app/static/js/main.c3b81cc7.chunk.js"
+  },
+  {
+    "revision": "13bb6962ec93851bb656",
+    "url": "/color-app/static/js/runtime-main.c6620004.js"
+  }
+]);
